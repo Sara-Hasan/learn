9b4d3750e962fa38aa63a2e2957d98388811a395
Sara-Hasan <sara.ahabash@gmail.com>
2022-01-18 02:07:08 +0200

    add

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('master')
+@section('content')
+<header>
+    <div class="header">
+        <div class="head">
+            <h1> ​Education and Online Course Site </h1>
+            <h3> ​Our c​ourses </h3>
+            <div class="buttons">
+                <div class="button"> <a href="#" > Course </a> </div>
+                <div class="button"> <a href="#" > Course </a> </div>
+                <div class="button"> <a href="#" > Course </a> </div>
+                <div class="button"> <a href="#" > Course </a> </div>
+            </div>
+        </div>
+    </div>  
+</header>
+@endsection
---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/media.css">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+
+    <title>  </title>
+</head>
+<body>
+<nav>
+    <input type="checkbox" id="check">
+    <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+    </label>
+    <label class="logo">
+        <a href="index.html">
+         {{-- <i class="fas fa-code"></i> --}}
+          Learn Arabic 
+         </a> 
+    </label>
+    <ul>
+        <li>
+            <a href="#About"> Home  </a> 
+        </li>
+        <li>
+            <a href="#About"> About us  </a> 
+        </li>
+        <li>
+            <a class="contact" href="#Contact">  Contact us </a>
+        </li>
+        <li> 
+            <a href="#Skill">  Course </a>
+        </li>
+        <li> 
+            <a href="#Service">  Instructor </a>
+        </li>
+        <li> 
+            <a href="#Portfolio">  Register </a>
+        </li>
+        <li> 
+            <a href="#Portfolio">  Login </a>
+        </li>
+    </ul>
+</nav>

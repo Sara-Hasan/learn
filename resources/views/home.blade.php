be90de7f9985d95d45b080ab583ffd9fe2e15fc6
Sara-Hasan <sara.ahabash@gmail.com>
2022-01-19 02:20:31 +0200

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,4 +14,40 @@
         </div>
     </div>  
 </header>
+<section class="pick-course">
+    <div class="container">
+        <h4> ​START LEARNING TODAY </h4>
+        <h1> Pick Your Course </h1>
+        <div class="courses">
+            <div class="course">
+                <img src="../img/course1.jpeg" alt="">
+                <h5> ​Course name </h5>
+                <p> Teacher TONY GARRETT </p>
+                <div class="line"></div>
+                <span> ​$199                               2300 </span>
+            </div>
+            <div class="course">
+                <img src="../img/course1.jpeg" alt="">
+                <h5> ​Course name </h5>
+                <p> Teacher TONY GARRETT </p>
+                <div class="line"></div>
+                <span> ​$199                               2300 </span>
+            </div>
+            <div class="course">
+                <img src="../img/course1.jpeg" alt="">
+                <h5> ​Course name </h5>
+                <p> Teacher TONY GARRETT </p>
+                <div class="line"></div>
+                <span> ​$199                               2300 </span>
+            </div>
+            <div class="course">
+                <img src="../img/course1.jpeg" alt="">
+                <h5> ​Course name </h5>
+                <p> Teacher TONY GARRETT </p>
+                <div class="line"></div>
+                <span> ​$199                               2300 </span>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection

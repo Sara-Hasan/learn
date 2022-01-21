46d4b6463f7acfcd04f0b42515b717276e0861e2
Sara-Hasan <sara.ahabash@gmail.com>
2022-01-21 04:09:40 +0200

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,10 @@
 @section('content')
 <header>
     <div class="header">
-        <div class="head">
+        <div class="head text-align">
             <h1> ​Education and Online Course Site </h1>
             <h5> ​Our c​ourses </h5>
-            <div class="buttons">
+            <div class="buttons d-flex">
                 <div class="button"> <a href="#" > Course </a> </div>
                 <div class="button"> <a href="#" > Course </a> </div>
                 <div class="button"> <a href="#" > Course </a> </div>
@@ -16,31 +16,31 @@
 </header>
 <section class="pick-course">
     <div class="container">
-        <h4> ​START LEARNING TODAY </h4>
+        <h4 class="text-align"> ​START LEARNING TODAY </h4>
         <h1> Pick Your Course </h1>
         <div class="courses">
-            <div class="course">
+            <div class="course text-align">
                 <div class="picture" style="background-image: url('../img/course1.jpeg');"></div>
                 <h5> ​Course name </h5>
                 <p> Teacher TONY GARRETT </p>
                 <div class="line"></div>
                 <span> ​$199                               2300 </span>
             </div>
-            <div class="course">
+            <div class="course text-align">
                 <div class="picture" style="background-image: url('../img/course1.jpeg');"></div>
                 <h5> ​Course name </h5>
                 <p> Teacher TONY GARRETT </p>
                 <div class="line"></div>
                 <span> ​$199                               2300 </span>
             </div>
-            <div class="course">
+            <div class="course text-align">
                 <div class="picture" style="background-image: url('../img/course1.jpeg');"></div>
                 <h5> ​Course name </h5>
                 <p> Teacher TONY GARRETT </p>
                 <div class="line"></div>
                 <span> ​$199                               2300 </span>
             </div>
-            <div class="course">
+            <div class="course text-align">
                 <div class="picture" style="background-image: url('../img/course1.jpeg');"></div>
                 <h5> ​Course name </h5>
                 <p> Teacher TONY GARRETT </p>
@@ -55,7 +55,7 @@
         <div class="container">
             <div class="row">
                 <div class="in"> 
-                    <div class="col">
+                    <div class="col d-flex text-align">
                         <div class="icon">
                             <i class="fas fa-chalkboard-teacher"></i>
                         </div>
@@ -66,7 +66,7 @@
                     </div>
                 </div>
                 <div class="in"> 
-                    <div class="col">
+                    <div class="col d-flex text-align">
                         <div class="icon">
                             <i class="fas fa-chalkboard-teacher"></i>
                         </div>
@@ -77,7 +77,7 @@
                     </div>
                 </div>
                 <div class="in"> 
-                    <div class="col">
+                    <div class="col d-flex text-align">
                         <div class="icon">
                             <i class="fas fa-chalkboard-teacher"></i>
                         </div>
@@ -88,7 +88,7 @@
                     </div>
                 </div>
                 <div class="in"> 
-                    <div class="col">
+                    <div class="col d-flex text-align">
                         <div class="icon">
                             <i class="fas fa-chalkboard-teacher"></i>
                         </div>
@@ -117,22 +117,22 @@
              </div>
         </div>
         <div class="row-sec">
-            <div class="col-sec">
-                <div> <i class="far fa-lightbulb"></i> </div>
+            <div class="col-sec text-align">
+                <div class="d-flex"> <i class="far fa-lightbulb"></i> </div>
                 <h5> ​How We Teach </h5>
                 <p>Duis aute irure dolor in reprehenderit in voluptate velit esse 
                     cillum dolore eu fugiat nulla pariatur</p>   
                 <span> <a href="#"> More </a> </span>         
             </div>
-            <div class="col-sec">
-                <div><i class="fas fa-medal"></i></div>
+            <div class="col-sec text-align">
+                <div class="d-flex"><i class="fas fa-medal"></i></div>
                 <h5> Results </h5>
                 <p>Duis aute irure dolor in reprehenderit in voluptate velit esse 
                     cillum dolore eu fugiat nulla pariatur</p>   
                 <span> <a href="#"> More </a> </span>                      
             </div>
-             <div class="col-sec">
-                <div><i class="fas fa-question-circle"></i></div>
+             <div class="col-sec text-align">
+                <div class="d-flex"><i class="fas fa-question-circle"></i></div>
                 <h5> Support </h5>
                 <p>Duis aute irure dolor in reprehenderit in voluptate velit esse 
                     cillum dolore eu fugiat nulla pariatur</p>   
@@ -142,4 +142,91 @@
         
     </div>
 </section>
+
+<section class="testimony-section">
+    <div class="overlay" style="background-image: url('../img/bg_2.jpg');"></div>
+    <div class="container">
+        <h6 class="subheading text-align">Testimonial</h6>
+        <h2>What Are Students Says</h2>
+            <div class="item d-flex">
+                <div class="testimony-wrap">
+                    <div class="text">
+                        <p class="star">
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                        </p>
+                        <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                        <div class="d-flex align-items-center">
+                        <div class="user-img" style="background-image: url(../img/person_2.jpg)"></div>
+                        <div class="pl-3">
+                            <p class="name">Roger Scott</p>
+                            <span class="position">Marketing Manager</span>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="testimony-wrap">
+                    <div class="text">
+                        <p class="star">
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                        </p>
+                        <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                        <div class="d-flex align-items-center">
+                        <div class="user-img" style="background-image: url(../img/person_2.jpg)"></div>
+                        <div class="pl-3">
+                            <p class="name">Roger Scott</p>
+                            <span class="position">Marketing Manager</span>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="testimony-wrap">
+                    <div class="text">
+                        <p class="star">
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                        </p>
+                        <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                        <div class="d-flex align-items-center">
+                        <div class="user-img" style="background-image: url(../img/person_2.jpg)"></div>
+                        <div class="pl-3">
+                            <p class="name">Roger Scott</p>
+                            <span class="position">Marketing Manager</span>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="testimony-wrap">
+                    <div class="text">
+                        <p class="star">
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                        </p>
+                        <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                        <div class="d-flex align-items-center">
+                        <div class="user-img" style="background-image: url(../img/person_2.jpg)"></div>
+                        <div class="pl-3">
+                            <p class="name">Roger Scott</p>
+                            <span class="position">Marketing Manager</span>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+    </div>
+ </section>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,9 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('home');
 });
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
